255b88402472ee52e95c50202a0c970b8d37b549
Dustin.Liu <e2755699@gmail.com>
2024-08-17 21:10:38 +0800

    add on foucs desgin

--- packages/ui_component/lib/src/forms/yr_form_field.dart
+++ packages/ui_component/lib/src/forms/yr_form_field.dart
@@ -4,38 +4,53 @@ import 'package:flutter/material.dart';
 import 'package:ui_component/src/design_token.dart';
 import 'package:ui_component/src/forms/yr_text.dart';
 
-class YrFormField extends StatelessWidget {
+class YrFormField extends StatefulWidget {
   final String data;
 
   const YrFormField(this.data, {super.key});
 
   @override
+  State<YrFormField> createState() => _YrFormFieldState();
+}
+
+class _YrFormFieldState extends State<YrFormField> {
+  final FocusScopeNode focusScopeNode = FocusScopeNode();
+
+  @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: YrDesignToken.form.width,
-      child: Column(
-        children: [
-          YrText(
-            data: data,
-            color: YrDesignToken.form.color,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderSide: BorderSide(color: YrDesignToken.form.color),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: YrDesignToken.form.color),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: YrDesignToken.form.color),
-              ),
+    return FocusScope(
+      node: focusScopeNode,
+      onFocusChange: (bool onFocus) {
+        setState(() {});
+
+      },
+      child: SizedBox(
+        width: YrDesignToken.form.width,
+        child: Column(
+          children: [
+            YrText(
+              data: widget.data,
+              color: focusScopeNode.hasFocus
+                  ? YrDesignToken.form.color
+                  : Colors.grey,
             ),
-            cursorColor: YrDesignToken.form.color,
-          ),
-        ],
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: YrDesignToken.form.color),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: YrDesignToken.form.color),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: YrDesignToken.form.color),
+                ),
+              ),
+              cursorColor: YrDesignToken.form.color,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
